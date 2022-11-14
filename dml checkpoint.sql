77e19aab9c3b867e360ef9a8b4fe7dbc6a535fa4
joshuaonyibe24 <114414817+joshuaonyibe24@users.noreply.github.com>
2022-11-14 02:08:41 -0800

    Add files via upload

---
+++ dml checkpoint.sql
@@ -0,0 +1,27 @@
+insert into customer ( customer_id,customer_name,customer_tel) values
+	('CO1','asama','77345823');
+	
+	
+	
+insert into customer ( customer_id,customer_name,customer_tel) values
+	('CO2','ali','71321009')	
+
+
+insert into product ( product_id,product_name,price,category) values
+	('po1','samsung galaxy s20','3299','smartphone')	
+	
+
+insert into product ( product_id,product_name,price,category) values
+	('pO2','asus notebook','4599','pc')	
+	
+
+insert into orders ( customer_id,product_id,amount,quantity,dob) values
+	('CO1','po2','9198','2','null')	
+	
+	
+	
+insert into orders ( customer_id,product_id,amount,quantity,dob) values
+	('CO2','po1','3299','1','28/05/2020')	
+	
+
+
